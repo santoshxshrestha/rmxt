a48808094220b23a2dd5e3ca6380fc39b849ed77
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-25 22:18:08 +0545

    basic functionality of the program

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@
 mod args;
 use args::Args;
 use clap::{Parser, parser};
+use std::fs::remove_file;
 
 fn main() {
     let args = Args::parse();
-
-    match args {
-        Args { file: f } => {
-            println!("File to be removed: {}", f);
-        }
+    let file_path = args.file;
+    if let Err(e) = remove_file(file_path) {
+        eprintln!("Error deleting file: {e}");
     }
 }
