504481073e4d4e39ff981c9da0bb599450fcf7d9
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-18 21:52:36 +0545

    update: flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,28 @@
 {
-  description = "A Nix-flake-based Rust development environment";
+  description = "A Nix-flake-based Rust development environment for rmxt )";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, naersk, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
+      naerskLib = pkgs.callPackage naersk { };
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustc
-          cargo
-          clippy
-          rustfmt
-          rust-analyzer
-          pkg-config
-        ];
-
-        shellHook = ''
-          export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
-        '';
+      packages.${system}.default = naerskLib.buildPackage {
+        src = ./.;
+        nativeBuildInputs = [ pkgs.pkg-config ];
       };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ rustc cargo clippy rustfmt rust-analyzer ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+
+        # env.RUST_SRC_PATH =
+        #   "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+      formatter = pkgs.rustfmt;
     };
 }
